ca85c457c5c2a4329c97720b9d419d71a80deec9
Anthony Najjar <anthonynsimon@gmail.com>
2016-11-30 13:38:46 +0100

    Add project head builds

--- app/Http/Controllers/API/BuildController.php
+++ app/Http/Controllers/API/BuildController.php
@@ -14,144 +14,138 @@ class BuildController extends Controller
 {
     public function index(Request $request)
     {
-		// Optional route query string
-		$platform = $request->input('platform');
-		$orderBy = $request->input('orderBy');
-		$orderType = $request->input('orderType');
-		
-		$builds = Build::select('*');
-			
-		if ($platform) {
-			$builds = $builds->where('platform', $platform);
-		}
-		if ($orderBy) {
-			$builds = $builds->orderBy($orderBy, $orderType ? $orderType : 'desc');
-		}
-		
-		$builds = $builds->get();
-		
-		return response()->json($builds, 200);
-	}
+        // Optional route query string
+        $platform = $request->input('platform');
+        $orderBy = $request->input('orderBy');
+        $orderType = $request->input('orderType');
+        
+        $builds = Build::select('*');
+            
+        if ($platform) {
+            $builds = $builds->where('platform', $platform);
+        }
+        if ($orderBy) {
+            $builds = $builds->orderBy($orderBy, $orderType ? $orderType : 'desc');
+        }
+        
+        $builds = $builds->get();
+        
+        return response()->json($builds, 200);
+    }
 
     public function store(Request $request)
     {
-        try
-        {
-			$input = $request->all();
-			
-			$validator = Validator::make($input, [
-				'projectIdent' => 'required',
-				'bundleIdentifier' => 'required',
-				'installFolder' => 'required',
-				'installFileName' => 'required',
-				'version' => 'required',
-				'buildNumber' => 'required',
-				'platform' => 'required',
-				'revision' => 'required'
-			]);
-			
-			if ($validator->fails()) {
-				return response()->json([
-					'errors' => $validator->errors()
-				], 400);
-			}
-			
-            $project = Project::where('ident', '=', $input['projectIdent'])->first();
-			
-			if (!$project) {
-				throw new CustomException("Provided project id or name not found, "
-											."you must specify an existing project "
-											."to associate with this built", 404);
-			}
+        try {
+            $input = $request->all();
+            
+            $validator = Validator::make($input, [
+                'projectIdent' => 'required',
+                'bundleIdentifier' => 'required',
+                'installFolder' => 'required',
+                'installFileName' => 'required',
+                'version' => 'required',
+                'buildNumber' => 'required',
+                'platform' => 'required',
+                'revision' => 'required'
+            ]);
+            
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors' => $validator->errors()
+                ], 400);
+            }
 
-			$build = $project->builds()->create($input);
+            $platformMappings = array(
+                'iphone' => 'ios',
+                'ipad' => 'ios',
+                'ios' => 'ios',
+                'android' => 'android'
+            );
+        
+            $input->platform = $platformMappings[strtolower($input->platform)];
+            
+            $project = Project::where('ident', '=', $input['projectIdent'])->first();
+            
+            if (!$project) {
+                throw new CustomException("Provided project id or name not found, "
+                                            ."you must specify an existing project "
+                                            ."to associate with this built", 404);
+            }
+
+            $build = $project->builds()->create($input);
 
             return response()->json($build, 200);
-        }
-		catch (\Exception $e)
-        {
-			$statusCode = $e instanceof CustomException ? $e->getCode() : 500;
+        } catch (\Exception $e) {
+            $statusCode = $e instanceof CustomException ? $e->getCode() : 500;
             return response()->json(['errors' => ['message' => $e->getMessage()]], $statusCode);
         }
     }
 
     public function show($id)
     {
-		try
-        {
+        try {
             $build = Build::find($id);
-			
-			if (!$build) {
-				throw new CustomException("Provided build id not found", 404);
-			}
-			
-			return response()->json($build, 200);
-        }
-		catch (\Exception $e)
-        {
-			$statusCode = $e instanceof CustomException ? $e->getCode() : 500;
-            return response()->json(['errors' => ['message' => $e->getMessage()]], $statusCode);
-        }
-	}
-	
-	public function getProject($id)
-    {
-		try
-        {
-            $build = Build::find($id);
-			
-			if (!$build) {
-				throw new CustomException("Provided build id not found",404);
-			}
-			
-			return response()->json($build->project, 200);
-        }
-		catch (\Exception $e)
-        {
-			$statusCode = $e instanceof CustomException ? $e->getCode() : 500;
-            return response()->json(['errors' => ['message' => $e->getMessage()]], $statusCode);
-        }
-	}
-
-    public function update(Request $request, $id)
-    {
-		try
-        {
-            $build = Build::find($id);
-			
-			if (!$build) {
-				throw new CustomException("Provided build id not found",404);
-			}
-			
-			$build->update($request->all());
-			
+            
+            if (!$build) {
+                throw new CustomException("Provided build id not found", 404);
+            }
+            
             return response()->json($build, 200);
-        }
-		catch (\Exception $e)
-        {
-			$statusCode = $e instanceof CustomException ? $e->getCode() : 500;
+        } catch (\Exception $e) {
+            $statusCode = $e instanceof CustomException ? $e->getCode() : 500;
             return response()->json(['errors' => ['message' => $e->getMessage()]], $statusCode);
         }
     }
-	
-	public function destroy($id)
+    
+    public function getProject($id)
     {
-		try
-        {
+        try {
             $build = Build::find($id);
-			
-			if (!$build) {
-				throw new CustomException("Provided build id not found",404);
-			}
-			
-			$build->delete();
-			
-            return response()->json($build, 200);
-        }
-		catch (\Exception $e)
-        {
-			$statusCode = $e instanceof CustomException ? $e->getCode() : 500;
+            
+            if (!$build) {
+                throw new CustomException("Provided build id not found", 404);
+            }
+            
+            return response()->json($build->project, 200);
+        } catch (\Exception $e) {
+            $statusCode = $e instanceof CustomException ? $e->getCode() : 500;
             return response()->json(['errors' => ['message' => $e->getMessage()]], $statusCode);
         }
-	}
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $build = Build::find($id);
+            
+            if (!$build) {
+                throw new CustomException("Provided build id not found", 404);
+            }
+            
+            $build->update($request->all());
+            
+            return response()->json($build, 200);
+        } catch (\Exception $e) {
+            $statusCode = $e instanceof CustomException ? $e->getCode() : 500;
+            return response()->json(['errors' => ['message' => $e->getMessage()]], $statusCode);
+        }
+    }
+    
+    public function destroy($id)
+    {
+        try {
+            $build = Build::find($id);
+            
+            if (!$build) {
+                throw new CustomException("Provided build id not found", 404);
+            }
+            
+            $build->delete();
+            
+            return response()->json($build, 200);
+        } catch (\Exception $e) {
+            $statusCode = $e instanceof CustomException ? $e->getCode() : 500;
+            return response()->json(['errors' => ['message' => $e->getMessage()]], $statusCode);
+        }
+    }
 }
---
+++ resources/views/shared/build.blade.php
@@ -0,0 +1,80 @@
+@if (isset($build))
+<div class="card soft-shadow">
+    <div class="card-header text-white bg-primary">
+        <div class="row">
+            <div class="col-md-12">
+                <label><h5>Build # {{$build->buildNumber}}</h5></label>
+                <div class="btn-group pull-xs-right">
+                    @if (strtolower($build->platform) == 'android')
+                    <a href="{!!url('/downloads/builds/'.$build->id)!!}"
+                        class="btn btn-success btn-sm">
+                        Install
+                    </a>
+                    @elseif (strtolower($build->platform) == 'ios')
+                    <a href="itms-services://?action=download-manifest&url={!!url('/downloads/plist/'.ViewService::generateUrlSafeToken($build->id))!!}" 
+                        class="btn btn-success btn-sm">
+                        Install
+                    </a>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <br>
+        <div class="table-responsive">
+            <table class="table table-bordered table-sm">
+                <tbody>
+                    <tr>
+                        <th>Revision</th>
+                        <td>{{$build->revision or 'N/A'}}</td>
+                    </tr>
+                    <tr>
+                        <th>Platform</th>
+                        <td>{{$build->platform or 'N/A'}}</td>
+                    </tr>
+                    <tr>
+                        <th>Bundle Identifier</th>
+                        <td>{{$build->bundleIdentifier or 'N/A'}}</td>
+                    </tr>
+                    <tr>
+                        <th>Version</th>
+                        <td>{{$build->version or 'N/A'}}</td>
+                    </tr>
+                    <tr>
+                        <th>ID</th>
+                        <td>{{$build->id or 'N/A'}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        @if (Auth::user()->can('adminOnly'))
+            <strong>Build note:</strong>
+            <form method="POST" action="{{ url('/projects/' . $projectId . '/builds/' . $build->id . '/note') }}">
+                {!! csrf_field() !!}
+                <fieldset class="form-group">
+                    <input name="_method" type="hidden" value="PATCH">
+
+                    <textarea id="editableTextArea" readonly type="text" class="form-control notEditing" name="note" placeholder="Click to edit your build note...">{{$build->note}}</textarea>
+                    
+                    <br />
+                    <button id="editableTextAreaSubmit" type="submit" hidden class="btn btn-sm btn-primary">Update note</button>
+                    <button id="editableTextAreaCancel" type="button" hidden class="btn btn-sm btn-danger">Cancel changes</button>
+                </fieldset>
+            </form>
+        @else
+        <div class="alert alert-warning">
+            <strong>Build note:</strong>
+            <p class="wrap-text">{{empty($build->note) ? "No note available for this build." : $build->note}}</p>
+        </div>
+        @endif
+    </div>
+
+    <div class="card-footer text-muted">Received at {{date_format($build->created_at, 'G:i \o\n l jS F Y')}}</div>
+    
+</div>
+@endif
